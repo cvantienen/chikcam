2de954f7f69738670412790ec8c8d46f31240ad0
cvantienen <codyvantienen@gmail.com>
2024-06-11 12:00:55 -0400

    Stripe webhook change

--- chikcam/billing/views.py
+++ chikcam/billing/views.py
@@ -63,7 +63,7 @@ def stripe_webhook(request: HttpRequest):
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_TEST_HOOK
+            payload, sig_header, settings.STRIPE_WEB_HOOK
         )
     except ValueError as e:
         # Invalid payload
