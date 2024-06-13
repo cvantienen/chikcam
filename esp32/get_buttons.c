83c86dae540ff550853d223f7674389a9c3574a8
codyvantienen <codyvantienen@gmail.com>
2024-06-12 20:27:52 -0400

    Led removed, servo added

--- esp32/get_buttons.c
+++ esp32/get_buttons.c
@@ -2,19 +2,13 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ESP32Servo.h>
-#include <FastLED.h>
 
-#define NUM_LEDS 8      /* The amount of pixels/leds you have */
-#define DATA_PIN 14      /* The pin your data line is connected to */
-#define LED_TYPE WS2812B /* I assume you have WS2812B leds, if not just change it to whatever you have */
-#define BRIGHTNESS 255   /* Control the brightness of your leds */
-#define SATURATION 255   /* Control the saturation of your leds */
+#define RELAY_PIN 33 // ESP32 pin GPIO16 connected to the IN pin of relay
 
-CRGB leds[NUM_LEDS];
 
 Servo myservo;  // create servo object to control a servo
 
-int pos = 0; 
+int pos = 0;
 
 const char* ssid = "BASIL";
 const char* password = "fluffypotato";
@@ -23,15 +17,13 @@ const int servoPin = 26;  // GPIO26
 
 int prevActivationCounts[5] = {0}; // Store previous activation counts for 5 activation types
 
-const char* actionTypeStrings[] = {"take_pic", "flashlight", "feed_snack", "text_to_talk", "play_song"};
+const char* actionTypeStrings[] = { "take_pic", "text_to_talk", "play_song", "flashlight", "feed_snack",};
 
+unsigned long previousFeedTime = 0; // Keep track of the last time feedSnack was called
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-
-  // setup LED
-  FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);
 
   // Connect to Wi-Fi network
   connectToWiFi();
@@ -39,25 +31,55 @@ void setup() {
   printNetworkConfig();
 
   // Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
-	// using default min/max of 1000us and 2000us
-	// different servos may require different min/max settings
-	// for an accurate 0 to 180 sweep
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.attach(servoPin, 500, 2500); // attaches the servo on pin 18 to the servo object
+  // different servos may require different min/max settings
+  myservo.setPeriodHertz(50);    // standard 50 hz servo
+
+  pinMode(RELAY_PIN, OUTPUT);
 }
 
 void loop() {
-  delay(100);
+  unsigned long currentMillis = millis();
 
-  feedSnack();
+  if (currentMillis - previousFeedTime >= 10000) {
+    previousFeedTime = currentMillis;
+    makeRequest();
+  }
+}
 
-  rainbowLed();
-  
-  
+void connectToWiFi() {
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 25) {
+    delay(1000);
+    Serial.println("Connecting...");
+    attempts++;
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to WiFi");
+  } else {
+    Serial.println("Failed to connect to WiFi");
+  }
+}
+
+void printNetworkConfig() {
+  Serial.println("Network Configuration:");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Subnet Mask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway IP: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("DNS Server: ");
+  Serial.println(WiFi.dnsIP());
+}
+
+void makeRequest() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
@@ -93,9 +115,11 @@ void loop() {
           Serial.println(activationCount);
           Serial.println();
 
-                    // Check if activation count increased for the current action type
+          // Check if activation count increased for the current action type
           for (int i = 0; i < 5; i++) {
             if (strcmp(actionType, actionTypeStrings[i]) == 0 && activationCount > prevActivationCounts[i]) {
+              Serial.println(actionType);
+              Serial.println(i);
               // Call corresponding function based on action type
               activateFunction(i);
               // Update previous activation count
@@ -113,60 +137,29 @@ void loop() {
     Serial.println("WiFi Disconnected");
     connectToWiFi();
   }
-  feedSnack();
-  delay(5000); // Wait for 5 seconds before making the next request
 }
-
-void connectToWiFi() {
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-  int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 25) {
-    delay(1000);
-    Serial.println("Connecting...");
-    attempts++;
-  }
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected to WiFi");
-  } else {
-    Serial.println("Failed to connect to WiFi");
-  }
-}
-
-void printNetworkConfig() {
-  Serial.println("Network Configuration:");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Subnet Mask: ");
-  Serial.println(WiFi.subnetMask());
-  Serial.print("Gateway IP: ");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("DNS Server: ");
-  Serial.println(WiFi.dnsIP());
-}
-
 
 void activateFunction(int actionIndex) {
   switch(actionIndex) {
     case 0:
-      // Activate function for take_pic
-      takePicture();
+      // Activate function for flashlight
+      textToTalk();
       break;
     case 1:
-      // Activate function for flashlight
-      turnOnFlashlight();
+      // Activate function for text_to_talk
+      takePic();
       break;
     case 2:
-      // Activate function for feed_snack
-      feedSnack();
+        // Activate function for play_song
+      playSong();
       break;
     case 3:
-      // Activate function for text_to_talk
-    textToTalk();
+      // Activate function for take_pic
+      turnOnFlashlight();
       break;
     case 4:
-      // Activate function for play_song
-      playSong();
+      // Activate function for feed_snack
+      feedSnack();
       break;
     default:
       // Handle unexpected action index
@@ -175,8 +168,7 @@ void activateFunction(int actionIndex) {
 }
 
 // Activation functions for each action type
-
-void takePicture() {
+void takePic() {
   // Add your logic to take a picture
   Serial.println("Taking a picture");
   // Implement your code to take a picture here
@@ -185,23 +177,32 @@ void takePicture() {
 void turnOnFlashlight() {
   // Add your logic to turn on the flashlight
   Serial.println("Turning on flashlight");
-  // Implement your code to turn on the flashlight here
+  // Show the LEDs after setting white
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(15000);
+  digitalWrite(RELAY_PIN, LOW);
+  delay(100);
 }
 
 void feedSnack() {
   // Add your logic to feed a snack
   Serial.println("Feeding snack");
-  // Sweep the servo back and forth
-  for (pos = 0; pos <= 230; pos++) {
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);           // waits 15ms for the servo to reach the position
+
+  // Rotate the servo from 0 to 180 degrees
+  for (int angle = 0; angle <= 180; angle++) {
+    int pulseWidth = map(angle, 0, 180, 500, 2500);
+    myservo.writeMicroseconds(pulseWidth);
+    delay(8);
   }
-  
-  // Rotate from 230 to 0 degrees
-  for (pos = 230; pos >= 0; pos--) {
-    myservo.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15); 
+
+  // Rotate the servo from 180 to 0 degrees
+  for (int angle = 180; angle >= 0; angle--) {
+    int pulseWidth = map(angle, 0, 180, 500, 2500);
+    myservo.writeMicroseconds(pulseWidth);
+    delay(8);
   }
+
+  delay(100); // Optional delay
 }
 
 void textToTalk() {
@@ -214,14 +215,4 @@ void playSong() {
   // Add your logic to play a song
   Serial.println("Playing a song");
   // Implement your code to play a song here
-}
-
-void rainbowLed() {
-  for (int j = 0; j < 255; j++) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the value 4 the less fade there is and vice versa */ 
-    }
-    FastLED.show();
-    delay(25); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
-  }
 }
